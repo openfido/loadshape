a95697660339723f81f60bb56c1435e506f7a5b4
David P. Chassin <dchassin@slac.stanford.edu>
2022-02-03 16:06:54 -0800

    Add autotest

---
+++ autotest.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Generic python environment for OpenFIDO
+#
+
+export OPENFIDO_INPUT=$PWD/autotest
+export OPENFIDO_OUTPUT=$PWD/autotest/output
+mkdir -p $OPENFIDO_OUTPUT
+rm $OPENFIDO_OUTPUT/*
+
+echo '*** INPUTS ***'
+ls -l $OPENFIDO_INPUT
+
+python3 openfido.py
+
+echo '*** OUTPUTS ***'
+ls -l $OPENFIDO_OUTPUT
+
+echo '*** END ***'
+
